be89170b7c7b81cc2f6d4b143d1688784a07e202
dimas1509 <mdimasdimas0915@gmail.com>
2024-12-30 10:14:45 +0700

    initial project

---
+++ flutter_pilih_manis/README.md
@@ -0,0 +1,3 @@
+# flutter_pilih_manis
+
+A new Flutter project.
---
+++ flutter_pilih_manis/android/app/src/main/kotlin/com/example/flutter_pilih_manis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_pilih_manis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
